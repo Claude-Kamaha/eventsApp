e1925b2782c466583b07e4ba87ec1e7d729c692d
Claude-Kamaha <kclaudejoelle@gmail.com>
2022-01-21 13:28:31 +0100

    sorting

--- src/app/events/event-details/session-list/session-list.component.ts
+++ src/app/events/event-details/session-list/session-list.component.ts
@@ -13,30 +13,44 @@ export class SessionListComponent implements OnInit,OnChanges {
   @Input()
   filterBy!: string;
   visibleSessions: ISession[] | undefined =[]
+  @Input()
+  sorting:string | undefined
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.filterBy)
+    console.log(this.sorting)
   }
 ngOnChanges(): void {
 
     if(this.sessions){
-      this.filterSession(this.filterBy)
+      this.filterSession(this.filterBy);
+      this.sorting =='name'?this.visibleSessions?.sort(sortByNameAsc):this.visibleSessions?.sort(sortByVotesDesc)
     }
 }
 filterSession(filter:any){
-  console.log('hey1')
+
   if(filter === 'all'){
    this.visibleSessions=this.sessions?.slice(0)//slice(0) create a complete copy list of array
     //this.visibleSessions=this.sessions
-    console.log('hey2')
+
   }
   else{
 this.visibleSessions = this.sessions?.filter(session => {
-  console.log('hey3')
+
   return session.level.toLocaleLowerCase() === filter;
 })
   }
-  console.log('hey4')
+ 
 }
+}
+function sortByNameAsc(s1: ISession,s2: ISession){
+  if(s1.name > s2.name) return 1
+  else if (s1.name === s2.name) return 0
+  else return -1
+
+}
+function sortByVotesDesc(s1: ISession,s2: ISession){
+
+   return s2.voters.length - s1.voters.length;
+
 }
